0210b0cb9adf1cffc103fbd1a89482bbe0b6c3aa
ssuarezp <ssuarezp@unal.edu.co>
2023-10-01 12:45:10 -0500

    20231001 5

--- pregunta.py
+++ pregunta.py
@@ -10,16 +10,13 @@ import pandas as pd
 
 
 def clean_data():
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    
-    #Renombrar columna
+    df= pd.read_csv("solicitudes_credito.csv", sep=";")
     df.rename(columns={'Unnamed: 0':'index'},inplace=True)
     df.set_index('index',inplace=True)
 
-    #Limpieza
+    df.sexo = df.sexo.str.lower().astype(str).str.strip()
     df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.capitalize().str.strip()
     df.idea_negocio = df.idea_negocio.str.replace('-',' ', regex=False).str.replace('_',' ', regex=False).str.capitalize().str.strip()
-    df.sexo = df.sexo.str.lower().astype(str).str.strip()
     df.barrio = df.barrio.str.replace('_','-').str.replace('-',' ').str.lower()
     df.estrato = df.estrato.astype("str").str.capitalize()
     df.comuna_ciudadano = df.comuna_ciudadano.astype(str).str.capitalize()
@@ -27,9 +24,6 @@ def clean_data():
     df.monto_del_credito = df.monto_del_credito.str.replace(",","",regex=True).str.replace("$","",regex=True).str.strip()
     df.monto_del_credito = df.monto_del_credito.astype(float)
     df.línea_credito=df.línea_credito.str.replace('-',' ', regex=False).str.replace('_',' ', regex=False).str.capitalize().str.strip()
-
-    #Eliminación de nulos y duplicados
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
-    
     return df
