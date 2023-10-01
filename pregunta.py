43e5e94a5a6271b3ecce911e240ae0bc140cf674
ssuarezp <ssuarezp@unal.edu.co>
2023-10-01 12:39:55 -0500

    20231001 2

--- pregunta.py
+++ pregunta.py
@@ -14,20 +14,20 @@ import re
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
     
-    #Eliminar datos duplicados y faltantes
+    #Renombrar columna
+    df.rename(columns={'Unnamed: 0':'index'},inplace=True)
+    df.set_index('index',inplace=True)
+
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.capitalize().str.strip()
+    df.idea_negocio = df.idea_negocio.str.replace('-',' ', regex=False).str.replace('_',' ', regex=False).str.capitalize().str.strip()
+    df.sexo = df.sexo.str.lower().astype(str).str.strip()
+    df.barrio = df.barrio.str.replace('_','-').str.replace('-',' ').str.lower()
+    df.estrato = df.estrato.astype("str").str.capitalize()
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(str).str.capitalize()
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"], dayfirst = True)
+    df.monto_del_credito = df.monto_del_credito.str.replace(",","",regex=True).str.replace("$","",regex=True).str.strip()
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.línea_credito=df.línea_credito.str.replace('-',' ', regex=False).str.replace('_',' ', regex=False).str.capitalize().str.strip()
     df.dropna(inplace=True)
-    df.sexo = df.sexo.str.lower()
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
-    df.idea_negocio = [str.lower(idea.replace("_", " ").replace("-", " ")) for idea in df.idea_negocio]
-    df.barrio = [str.lower(barrio).replace("_", " ").replace("-", " ") for barrio in df.barrio]
-    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
-    df.estrato = df.estrato.astype(int)
-    df["línea_credito"] = [str.lower(linea.strip().replace("-", " ").replace("_", " ").replace(". ", ".")) for linea in
-                           df["línea_credito"]]
-    df.fecha_de_beneficio = [datetime.strptime(date, "%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", date))
-                             else datetime.strptime(date, "%Y/%m/%d")
-                             for date in df.fecha_de_beneficio]
-    df.monto_del_credito = [int(monto.replace("$ ", "").replace(".00", "").replace(",", "")) for monto in
-                            df.monto_del_credito]
     df.drop_duplicates(inplace=True)
     return df
